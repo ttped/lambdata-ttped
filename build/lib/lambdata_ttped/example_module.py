dc482c6c19053596368136532c6b3a9b909bb41d
Trevor Pedersen <trevr43@gmail.com>
2020-09-28 16:32:02 -0600

    Updated requirements

---
+++ build/lib/lambdata_ttped/example_module.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+from sklearn.model_selection import train_test_split
+
+def train_test_split(df, target, features_arr, test_size=.2):
+    '''Takes in a dateframe and returns a train test split with target variable and feature variables'''
+    X = df[features_arr]
+    y = df[target]
+
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=test_size, random_state=42)
+    return X_train, X_val, y_train, y_val
+
+def month_day_year_split(datetime_column):
+    '''Takes a column in datetime format and returns month, day, and year columns'''
+    month = datetime_column.dt.month
+    day = datetime_column.dt.day
+    year = datetime_column.dt.year
+    return month, day, year
+
+def train_test_split_date(df, target, features_arr, date_column_str, to_date):
+    X = df[features_arr]
+    y = df[target]
+
+    mask = df[df[date_column_str] <= to_date]
+    mask_not = ~mask
+
+    X_train = X[mask]
+    y_train = y[mask]
+
+    X_val = X[mask_not]
+    y_val = y[mask_not]
+
+    return X_train, X_val, y_train, y_val
--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import setuptools
 REQUIRED = [
     "numpy",
     "pandas",
-    "datetime",
     "sklearn"
 ]
 
@@ -13,19 +12,19 @@ with open("README.MD", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
-    name='lambdata-ttped',
+    name='lambdata_ttped',
     version='0.0.1',
     author="ttped",
-    description="A collect of data science functions",
+    description="A collection of data science functions",
     long_description=LONG_DESCRIPTION,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url='https://github.com/ttped/lambdata_ttped',
     packages=setuptools.find_packages(),
-    python_requires=">=3.6",
     install_requires=REQUIRED,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS independent'
-    ]
+        'Operating System :: OS Independent'
+    ],
+    python_requires=">=3.6",
 )
